4927e0aa9e48529b6ae75681c4bdfc118e572fe7
Mateusz Sikora <mateusz.sikora@nokia.com>
2017-10-27 19:12:14 +0200

    users schema and seed

--- src/server/plugins/pwa.js
+++ src/server/plugins/pwa.js
@@ -1,9 +1,19 @@
-
+import mongoose from 'mongoose';
+import routes from '../api/routes';
+import seed from '../seeds';
 
 /*eslint-env es6*/
 const plugin = {};
+const url = `mongodb://localhost:27017/hopeit`;
 
 plugin.register = function (server, options, next) {
+  mongoose.connect(url).then(() => {
+    seed();
+  }).catch((err) => {
+    console.error(err)
+    throw err;
+  });
+
   server.route({
     method: 'GET',
     path: '/sw.js',
@@ -11,6 +21,10 @@ plugin.register = function (server, options, next) {
       file: 'dist/sw.js'
     }
   });
+
+  server.route(routes);
+
+
   next();
 };
 
