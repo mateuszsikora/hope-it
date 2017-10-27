4a76ae64744c2a367a24d8dc7bb9ba25a2a0c312
Dominik Markiewicz <theresq@gmail.com>
2017-10-28 01:53:30 +0200

    nav menu + stud statystyk dotacji

--- web/src/client/components/payments.jsx
+++ web/src/client/components/payments.jsx
@@ -6,7 +6,7 @@ import Payment from './payment';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux'
 import * as actions from '../actions'
-import {List, Header} from 'semantic-ui-react'
+import {List, Header, Statistic} from 'semantic-ui-react'
 
 class Payments extends PureComponent {
   static propTypes = {
@@ -30,9 +30,37 @@ class Payments extends PureComponent {
    )
   }
 
+  renderStats(){
+    return (
+      <Statistic.Group>
+        <Statistic>
+          <Statistic.Value>15 225zł</Statistic.Value>
+          <Statistic.Label>Suma zbiórki</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value>520</Statistic.Value>
+          <Statistic.Label>Darowizny</Statistic.Label>
+        </Statistic>
+        <Statistic>
+            <Statistic.Value>123</Statistic.Value>
+            <Statistic.Label>Darczyńców</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value>123</Statistic.Value>
+          <Statistic.Label>Darowizny w tym miesiącu</Statistic.Label>
+        </Statistic>
+        <Statistic>
+          <Statistic.Value>80</Statistic.Value>
+          <Statistic.Label>Darczyńców w tym miesiącu</Statistic.Label>
+        </Statistic>
+      </Statistic.Group>
+    )
+  }
+
   render(){
     return (
       <div>
+          {this.renderStats()}
           {this.renderPaymentsList()}
       </div>
     )
