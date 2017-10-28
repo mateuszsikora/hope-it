2a157282a85aadfd63177f94b994381ad5396305
Dominik Markiewicz <theresq@gmail.com>
2017-10-28 15:39:01 +0200

    surveys

--- web/src/client/reducers/index.jsx
+++ web/src/client/reducers/index.jsx
@@ -15,7 +15,13 @@ const payments = (store, action) => {
 const surveys = (store, action) => {
   switch(action.type) {
     case 'RECEIVE_SURVEYS': return {...store, surveys: action.payload}
-    default: return {surveys: []}
+    default: return store || {surveys: []}
+  }
+}
+const answers = (store, action) => {
+  switch(action.type) {
+    case 'RECEIVE_ANSWERS': return {...store, answers: action.payload}
+    default: return store || {answers: []}
   }
 }
 
@@ -45,5 +51,6 @@ export default combineReducers({
   login,
   donors,
   surveys,
+  answers,
   routing: routerReducer
 });
