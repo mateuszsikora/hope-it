3740037bf796e442071ff833f9d328242e9f5486
Filip Płotnicki <fplotnicki@gmail.com>
2017-10-28 13:36:07 +0200

    header

--- mobile/lib/components/Survey.js
+++ mobile/lib/components/Survey.js
@@ -1,76 +1,10 @@
 // @flow
 
-/*
-import React, { Component } from 'react';
-import { View, Image } from 'react-native'; 
-import { DeckSwiper, Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
-import routes from './../routes';
-import { Link } from 'react-router-native';
-
-const cards = [{
-    text: 'Card One',
-    name: 'One',
-    image: require('./img/hold-on.jpg'),
-}, {
-    text: 'Card Two',
-    name: 'Two',
-    image: require('./img/hold-on.jpg'),
-
-}]
-
-class SurveyContent extends Component {
-    state = { messages: [] }
-
-
-
-    render() {
-        return (
-            <Content>
-                <View>
-                    <Text>Ankiety</Text>
-                    <DeckSwiper
-                        dataSource={cards}
-                        renderItem={item =>
-                            <Card style={{ elevation: 3 }}>
-                                <CardItem>
-                                    <Left>
-                                        <Thumbnail source={item.image} />
-                                        <Body>
-                                            <Text>{item.text}</Text>
-                                            <Text note>NativeBase</Text>
-                                        </Body>
-                                    </Left>
-                                </CardItem>
-                                <CardItem cardBody>
-                                    <Image style={{ height: 300, flex: 1 }} source={item.image} />
-                                </CardItem>
-                                <CardItem>
-                                    <Icon name="heart" style={{ color: '#ED4A6A' }} />
-                                    <Text>{item.name}</Text>
-                                </CardItem>
-                            </Card>
-                        } />
-                </View>
-            </Content>
-        );
-    }
-}
-
-export default function Survey () {
-  return (
-      <SurveyContent />
-  )
-}
-*/
-
-
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { Container, Button, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Right, Body, Icon } from 'native-base';
 import { serverUrl } from '../util'
 
-const cards = [
-];
 export default class DeckSwiperExample extends Component {
     state = {surveys: [
     {
