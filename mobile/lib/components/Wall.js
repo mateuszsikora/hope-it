cfb18bb3d687b246571d763e61eddeef08bfe1de
Filip Płotnicki <fplotnicki@gmail.com>
2017-10-28 05:45:32 +0200

    card

--- mobile/lib/components/Wall.js
+++ mobile/lib/components/Wall.js
@@ -1,8 +1,8 @@
 // @flow
 
 import React, { Component } from 'react';
-import { View } from 'react-native'; 
-import { Text, Content } from 'native-base';
+import { View, Image } from 'react-native'; 
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 import routes from './../routes';
 import { Link } from 'react-router-native';
 
@@ -10,7 +10,37 @@ export default function Wall () {
   return (
     <Content>
       <View>
-        <Text>Hello from the wall</Text>
+          <Card>
+              <CardItem>
+                <Left>
+                  <Thumbnail source={{uri: 'Image URL'}} />
+                  <Body>
+                    <Text>NativeBase</Text>
+                    <Text note>GeekyAnts</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem cardBody>
+                <Image source={{uri: 'Image URL'}} style={{height: 200, width: null, flex: 1}}/>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button transparent>
+                    <Icon active name="thumbs-up" />
+                    <Text>12 Likes</Text>
+                  </Button>
+                </Left>
+                <Body>
+                  <Button transparent>
+                    <Icon active name="chatbubbles" />
+                    <Text>4 Comments</Text>
+                  </Button>
+                </Body>
+                <Right>
+                  <Text>11h ago</Text>
+                </Right>
+              </CardItem>
+            </Card>
       </View>
     </Content>
   )
